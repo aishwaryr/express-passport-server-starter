898429593a2a04a0b97fb28804e65dd210fb4359
aishwaryr <araj713@gmail.com>
2018-04-14 22:57:10 +0530

    Removed dead code

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,20 +3,6 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const promisify = require("es6-promisify");
 const envs = process.env;
-// const mail = require("../handlers/mail");
-
-// exports.login = passport.authenticate("local", {
-//   failureRedirect: "/login",
-//   failureFlash: "Failed Login",
-//   successRedirect: "/",
-//   successFlash: "You are now logged in!",
-// });
-
-// exports.logout = (req, res) => {
-//   req.logout();
-//   req.flash("success", "You are now logged out!");
-//   res.redirect("/");
-// };
 
 const tokenForUser = user => {
   const timestamp = new Date().getTime();
@@ -66,70 +52,3 @@ exports.signup = async (req, res, next) => {
   });
   // res.send({ sucess: true });
 };
-
-// exports.forgot = async (req, res) => {
-//   // 1. See if user with that email exists
-//   const user = await User.findOne({ email: req.body.email });
-//   if (!user) {
-//     req.flash("error", "No account with that email exists.");
-//     return res.redirect("/login");
-//   }
-//   // 2. Set reset tokens and expiry on their account
-//   user.resetPasswordToken = crypto.randomBytes(20).toString("hex");
-//   user.resetPasswordExpires = Date.now() + 3600000;
-//   await user.save();
-//   // 3. Send them an email with the token
-//   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-//   mail.send({
-//     user,
-//     subject: "Password Reset",
-//     resetURL,
-//     filename: "password-reset",
-//   });
-//   req.flash("success", `You have been mailed a password reset link.`);
-//   // 4. redirect to login page
-//   res.redirect("/login");
-// };
-
-// exports.reset = async (req, res) => {
-//   // check for token and it's expired or not
-//   const user = await User.findOne({
-//     resetPasswordToken: req.params.token,
-//     resetPasswordExpires: { $gt: Date.now() },
-//   });
-//   if (!user) {
-//     req.flash("error", "Password reset request is invalid or has expired");
-//     return res.redirect("/login");
-//   }
-//   // if there is a user, show the reset pass form
-//   res.render("reset", { title: "Reset Your Password" });
-// };
-
-// exports.confirmedPasswords = (req, res, next) => {
-//   if (req.body.password === req.body["password-confirm"]) {
-//     next();
-//     return;
-//   }
-//   req.flash("error", "Passwords do not match");
-//   res.redirect("back");
-// };
-
-// exports.update = async (req, res) => {
-//   const user = await User.findOne({
-//     resetPasswordToken: req.params.token,
-//     resetPasswordExpires: { $gt: Date.now() },
-//   });
-//   if (!user) {
-//     req.flash("error", "Password reset request is invalid or has expired");
-//     return res.redirect("/login");
-//   }
-
-//   const setPassword = promisify(user.setPassword, user);
-//   await setPassword(req.body.password);
-//   user.resetPasswordToken = undefined;
-//   user.resetPasswordExpires = undefined;
-//   const updatedUser = await user.save();
-//   await req.login(updatedUser);
-//   req.flash("success", "Nice! Your Password has been reset! You are now logged in");
-//   res.redirect("/");
-// };
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
+**Try with Postman**
+
+or
+
+[Here](https://github.com/aishwaryr/react-client-auth-jwt) is a React-Redux client for this
+
 ## What is this?
 
 Express server with authentication using Passport, Jwt, Mongo
@@ -5,5 +11,3 @@ Express server with authentication using Passport, Jwt, Mongo
 ## Setup Mongo & JWT secret
 
 Rename variables.env.sample to variables.env and modify the values if needed.
-
-**Try with Postman**
